22845e0bf2557c3eaac9043a0e0627ef1e268287
LeniaTziraxou <leniatziraxou@gmail.com>
2025-05-03 13:11:41 +0300

    jangologin

--- jango/MyProject/mysite/gym/views.py
+++ jango/MyProject/mysite/gym/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('περιβαλλονχρηστη')  # άλλαξέ το με το όνομα της σελίδας μετά το login
+        else:
+            messages.error(request, 'Λάθος όνομα χρήστη ή κωδικός.')
+    return render(request, 'login.html')
