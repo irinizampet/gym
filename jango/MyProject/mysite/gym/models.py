b9deb928ba681d2e607f7bcece7909c839d71d1c
LeniaTziraxou <leniatziraxou@gmail.com>
2025-05-03 12:02:07 +0300

    user class

--- jango/MyProject/mysite/gym/models.py
+++ jango/MyProject/mysite/gym/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    username = models.CharField(max_length=20, unique=True)
+    password = models.CharField
+    pnumber = models.CharField
+    email = models.EmailField(unique=True)
+    reg_date = models.DateTimeField(auto_now_add=True)
