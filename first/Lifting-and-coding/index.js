21b4d7559404c9e1faa1176f63cdd39eb86896a3
LeniaTziraxou <leniatziraxou@gmail.com>
2025-05-28 12:03:15 +0300

    commit

---
+++ first/Lifting-and-coding/index.js
@@ -0,0 +1,235 @@
+const Flexibility1 = document.getElementById("Flexibility1");
+const Calisthenics1 = document.getElementById("Calisthenics1");
+const Swimming1 = document.getElementById("Swimming1");
+const Strength1 = document.getElementById("Strength1");
+const Gymnastics1 = document.getElementById("Gymnastics1");
+const Pilates1 = document.getElementById("Pilates1");
+const Fitness1 = document.getElementById("Fitness1");
+const Gymnastics2 = document.getElementById("Gymnastics2");
+const Calisthenics2 = document.getElementById("Calisthenics2");
+const Pilates2 = document.getElementById("Pilates2");
+const Boxing1 = document.getElementById("Boxing1");
+const Strength2 = document.getElementById("Strength2");
+const Totalbody1 = document.getElementById("Totalbody1");
+const Pilates3 = document.getElementById("Pilates3");
+const Swimming2 = document.getElementById("Swimming2");
+const Flexibility2 = document.getElementById("Flexibility2");
+const Fitness2 = document.getElementById("Fitness2");
+const Totalbody2 = document.getElementById("Totalbody2");
+const Fitness3 = document.getElementById("Fitness3");
+const Gymnastics3 = document.getElementById("Gymnastics3");
+const Calisthenics3 = document.getElementById("Calisthenics3");
+const Pilates4 = document.getElementById("Pilates4");
+const Boxing2 = document.getElementById("Boxing2");
+const Strength3 = document.getElementById("Strength3");
+const Flexibility3 = document.getElementById("Flexibility3");
+const Calisthenics4 = document.getElementById("Calisthenics4");
+const Strength4 = document.getElementById("Strength4");
+const Swimming3 = document.getElementById("Swimming3");
+const Gymnastics4 = document.getElementById("Gymnastics4");
+const Pilates5 = document.getElementById("Pilates5");
+const Opengym1 = document.getElementById("Opengym1");
+const Opengym2 = document.getElementById("Opengym2");
+const Opengym3 = document.getElementById("Opengym3");
+const Opengym4 = document.getElementById("Opengym4");
+const Totalbody3 = document.getElementById("Totalbody3");
+const Boxing3 = document.getElementById("Boxing3");
+const Fitness4 = document.getElementById("Fitness4");
+const Strength5 = document.getElementById("Strength5");
+
+
+
+const mySubmit = document.getElementById("mySubmit");
+const mySubmit2 = document.getElementById("mySubmit2");
+const mySubmit3 = document.getElementById("mySubmit3");
+const mySubmit4 = document.getElementById("mySubmit4");
+const mySubmit5 = document.getElementById("mySubmit5");
+const mySubmit6 = document.getElementById("mySubmit6");
+
+const message = document.getElementById("message");
+const message2 = document.getElementById("message2");
+const message3 = document.getElementById("message3");
+const message4 = document.getElementById("message4");
+const message5 = document.getElementById("message5");
+const message6 = document.getElementById("message6");
+
+
+mySubmit.onclick = function(){
+
+    if(Flexibility1.checked){
+           message1.textContent = 'Δήλωσες Flexibility 10:00 - 11:00';
+           alert ('Επιτυχής Δήλωση')
+    }
+    if(Calisthenics1.checked){
+        message1.textContent = 'Δήλωσες Calisthenics 11:00 - 12:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Strength1.checked){
+        message1.textContent = 'Δήλωσες Strength 13:00 - 14:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Swimming1.checked){
+        message1.textContent = 'Δήλωσες Swimming 17:00 - 18:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Gymnastics1.checked){
+        message1.textContent = 'Δήλωσες Gymnastics 19:00 - 20:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Pilates1.checked){
+        message1.textContent = 'Δήλωσες Pilates 20:00 - 21:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+}
+
+
+mySubmit2.onclick = function(){
+   
+    if(Fitness1.checked){
+        message2.textContent = 'Δήλωσες Fitness 10:00 - 11:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Gymnastics2.checked){
+        message2.textContent = 'Δήλωσες Gymnastics 12:00 - 13:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Calisthenics2.checked){
+        message2.textContent = 'Δήλωσες Calisthenics 17:00 - 18:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Pilates2.checked){
+        message2.textContent = 'Δήλωσες Pilates 18:00 - 19:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Boxing1.checked){
+        message2.textContent = 'Δήλωσες Boxing 19:00 - 20:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Strength2.checked){
+        message2.textContent = 'Δήλωσες Strength 21:00 - 22:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+}
+
+mySubmit3.onclick = function(){
+   
+    if(Totalbody1.checked){
+        message3.textContent = 'Δήλωσες Total Body 11:00 - 12:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Pilates3.checked){
+        message3.textContent = 'Δήλωσες Pilates 13:00 - 14:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Swimming2.checked){
+        message3.textContent = 'Δήλωσες Swimming 17:00 - 18:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Flexibility2.checked){
+        message3.textContent = 'Δήλωσες Flexibility 18:00 - 19:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Fitness2.checked){
+        message3.textContent = 'Δήλωσες Fitness 20:00 - 21:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Totalbody2.checked){
+        message3.textContent = 'Δήλωσες Total Body 21:00 - 22:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+}
+
+mySubmit4.onclick = function(){
+   
+    if(Fitness3.checked){
+        message4.textContent = 'Δήλωσες Fitness 10:00 - 11:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Gymnastics3.checked){
+        message4.textContent = 'Δήλωσες Gymnastics 12:00 - 13:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Calisthenics3.checked){
+        message4.textContent = 'Δήλωσες Calisthenics 17:00 - 18:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Pilates4.checked){
+        message4.textContent = 'Δήλωσες Pilates 18:00 - 19:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Boxing2.checked){
+        message4.textContent = 'Δήλωσες Boxing 19:00 - 20:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Strength3.checked){
+        message4.textContent = 'Δήλωσες Strength 21:00 - 22:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+
+}
+
+mySubmit5.onclick = function(){
+   
+    if(Flexibility3.checked){
+        message5.textContent = 'Δήλωσες Flexibility 10:00 - 11:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Calisthenics4.checked){
+        message5.textContent = 'Δήλωσες Calisthenics 11:00 - 12:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Strength4.checked){
+        message5.textContent = 'Δήλωσες Strength 13:00 - 14:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Swimming3.checked){
+        message5.textContent = 'Δήλωσες Swimming 17:00 - 18:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Gymnastics4.checked){
+        message5.textContent = 'Δήλωσες Gymnastics 19:00 - 20:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Pilates5.checked){
+        message5.textContent = 'Δήλωσες Pilates 20:00 - 21:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+
+}
+
+mySubmit6.onclick = function(){
+   
+    if(Opengym1.checked){
+        message6.textContent = 'Δήλωσες Open Gym 10:00 - 11:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Opengym2.checked){
+        message6.textContent = 'Δήλωσες Open Gym 11:00 - 12:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Opengym3.checked){
+        message6.textContent = 'Δήλωσες Open Gym 12:00 - 13:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Opengym4.checked){
+        message6.textContent = 'Δήλωσες Open Gym 13:00 - 14:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Totalbody3.checked){
+        message6.textContent = 'Δήλωσες Total Body 18:00 - 19:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Boxing3.checked){
+        message6.textContent = 'Δήλωσες Boxing 19:00 - 20:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Fitness4.checked){
+        message6.textContent = 'Δήλωσες Fitness 20:00 - 21:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+    if(Strength5.checked){
+        message6.textContent = 'Δήλωσες Strength 21:00 - 22:00';
+        alert ('Επιτυχής Δήλωση')
+    }
+
+}
