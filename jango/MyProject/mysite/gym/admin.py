9572656da78bda550d58b4732001b62faa226c77
LeniaTziraxou <leniatziraxou@gmail.com>
2025-06-26 18:22:09 +0300

    Revert "ola kala mexri edw vol.3"
    This reverts commit ed41f95ef57614e97ea8ec426a12b3d728cf219b.

--- jango/MyProject/mysite/gym/admin.py
+++ jango/MyProject/mysite/gym/admin.py
@@ -13,13 +13,13 @@ from .models import (
 
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ("id", "avail_participations")
+    list_display = ("sub_id", "avail_participations")
 
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ("tran_id", "user", "subscription", "amount", "tran_date")
-    list_filter  = ("subscription",)
+    list_display = ("tran_id", "sub", "amount", "tran_date")
+    list_filter = ("sub",)
     search_fields = ("tran_id",)
 
 
@@ -37,34 +37,34 @@ class MemberAdmin(admin.ModelAdmin):
         "sub",
         "tran",
     )
-    list_filter  = ("sub", "tran", "age")
+    list_filter = ("sub", "tran", "age")
     search_fields = ("username", "email", "name", "surname")
 
 
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display   = ("em_id", "name", "surname", "age", "phone_num", "email")
-    search_fields  = ("name", "surname", "email")
+    list_display = ("em_id", "name", "surname", "age", "phone_num", "email")
+    search_fields = ("name", "surname", "email")
 
 
 @admin.register(Secretary)
 class SecretaryAdmin(EmployeeAdmin):
-    """κληρονομεί όλη την εμφάνιση από EmployeeAdmin"""
+    """κληρονομεί την εμφάνιση από EmployeeAdmin"""
 
 
 @admin.register(Coach)
 class CoachAdmin(EmployeeAdmin):
-    """κληρονομεί όλη την εμφάνιση από EmployeeAdmin"""
+    """κληρονομεί την εμφάνιση από EmployeeAdmin"""
 
 
 @admin.register(GymClass)
 class ClassAdmin(admin.ModelAdmin):
-    list_display   = ("cl_id", "name", "dates", "em", "capacity")
-    list_filter    = ("em", "dates")
-    search_fields  = ("name", "description")
+    list_display = ("cl_id", "name", "dates", "em", "capacity")
+    list_filter = ("em", "dates")
+    search_fields = ("name", "description")
 
 
 @admin.register(History)
 class HistoryAdmin(admin.ModelAdmin):
     list_display = ("h_id", "user", "cl", "date")
-    list_filter  = ("user", "cl", "date")
+    list_filter = ("user", "cl", "date")
